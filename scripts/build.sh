b94f18d4e596e4b50363eb57332e7ded2d8a844d
Jean-Marcel Belmont <marcelbelmont@gmail.com>
2017-07-31 15:46:01 -0400

    Change test script in npm to add reporter for cobertura and to generate lcov-report.

---
+++ scripts/build.sh
@@ -0,0 +1,14 @@
+# Automatically terminate if command returns error exit code
+set -e
+
+##### Setup .npmrc to use artifactory #####
+source ./repo/scripts/prebuild.sh
+
+cd $WORKSPACE/repo
+
+# run NPM install
+npm install
+
+## Run npm test
+npm test
+
