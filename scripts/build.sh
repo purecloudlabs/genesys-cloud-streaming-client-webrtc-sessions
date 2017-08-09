c2fda6cae14cdc5833799fc649ad0537aa18601b
Jean-Marcel Belmont <marcelbelmont@gmail.com>
2017-08-09 10:28:52 -0400

    Add build trigger to Jenkins Job.

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,7 +3,7 @@ set -e
 
 ##### Setup .npmrc to use artifactory #####
 source ./repo/scripts/prebuild.sh
-# 
+#
 cd $WORKSPACE/repo
 
 # run NPM install
@@ -11,13 +11,3 @@ npm install
 
 ## Run npm test
 npm test
-
-# Remove the pre-push hook during git tagging.
-# This was added during npm install and pre-push package.
-rm -f .git/hooks/pre-push
-
-# Publish the module
-./npm-utils/scripts/version-and-publish.sh
-
-# Push the tag
-git push origin --tags
