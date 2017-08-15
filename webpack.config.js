f052edbd84372e008eb958de1768567870015060
Jean-Marcel Belmont <marcelbelmont@gmail.com>
2017-08-15 13:48:00 -0400

    [FS-64] Add compilation step into firehose-webrtc-sessions.

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'firehose-webrtc-sessions.js',
+    library: 'firehose-webrtc-sessions',
+    libraryTarget: 'umd'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  }
+};
