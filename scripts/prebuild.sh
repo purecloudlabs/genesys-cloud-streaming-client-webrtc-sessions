29f3b2d37901c083f1981c8957d5731c5aaf6983
Xander Dumaine <xander.dumaine@inin.com>
2018-10-18 01:38:44 -0400

    Delete old scripts

--- scripts/prebuild.sh
+++
@@ -1,5 +0,0 @@
-cd $WORKSPACE/repo
-
-# Generate .npmrc for artifactory
-rm -rf ./npm-utils && git clone --depth=1 git@bitbucket.org:inindca/npm-utils.git ./npm-utils
-source ./npm-utils/scripts/jenkins-pre-build.sh ${NODE_VERSION} -m
