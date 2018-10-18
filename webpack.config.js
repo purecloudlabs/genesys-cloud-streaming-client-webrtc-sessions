85e8d8040483f76ab9d63ea7b56885cf64d0e97c
Xander Dumaine <xander.dumaine@inin.com>
2018-10-18 01:58:22 -0400

    Fix issues with webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,16 +3,17 @@ const path = require('path');
 module.exports = (env) => {
   const minimize = env && env.production;
   return {
+    target: 'node',
     entry: './src/index.js',
+    devtool: 'source-map',
     mode: minimize ? 'production' : 'development',
     optimization: {
       minimize
     },
-    devtool: 'source-map',
     output: {
       path: path.resolve(__dirname, 'dist'),
       filename: minimize ? 'webrtc-sessions.min.js' : 'webrtc-sessions.js',
-      library: 'pc-streaming',
+      library: 'webrtc-sessions',
       libraryTarget: 'umd'
     },
     module: {
